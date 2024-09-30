83cf410e15cd71ea1198b20250a360db48bf240b
Omkar Kathile <157679976+OmkarKathile007@users.noreply.github.com>
2024-09-30 23:54:23 +0530

    Update App.jsx

--- App/Frontend/src/App.jsx
+++ App/Frontend/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
           <Routes>
             <Route path="/"  element={<Home/>} />
             <Route path="/main" element={<Main/>} />
-               <Route path="/aiquiz" element={<AIQuiz/>}/>
+              
                <Route path="/mcqtest" element={<MCQTest/>} />
                <Route path="/videoquiz" element={<VideoQuiz/>} />
                <Route path="/userdashboard" element={<UserDashBoard/>} />
